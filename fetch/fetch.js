68c4a282a0d350ddd4de74fa06b2e7dd06234713
PranviComputech <pranvicomputech@gmail.com>
2023-04-01 14:30:56 +0530

    initial commit

---
+++ fetch/fetch.js
@@ -0,0 +1,30 @@
+//import modules
+const express = require('express')
+let mongodb = require('mongodb')
+//import url
+let url = require('../url')
+//create mongo client
+let mcl = mongodb.MongoClient
+//create router instance
+let router = express.Router()
+//create rest api
+router.get("/", (req, res) => {
+    //connect to mongodb
+    mcl.connect(url,(err, conn)=>{
+        if(err)
+            console.log(`Error in connection:- `,err)
+        else{
+            let db = conn.db("nodedb")
+            db.collection('products').find().toArray((err, array)=>{
+                if(err)
+                    console.log('Error :- '+err)
+                else{
+                    console.log('Data sent')
+                    res.json(array)                    
+                }
+            })
+        }
+    })
+})
+//export router
+module.exports = router
---
+++ url.js
@@ -0,0 +1,2 @@
+//module.exports = `mongodb://localhost:27017`
+module.exports = `mongodb+srv://admin:admin@mdb.vtkja.mongodb.net/?retryWrites=true&w=majority`
